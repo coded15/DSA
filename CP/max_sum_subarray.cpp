15f1c01e501df6612df203d3d76fafee66901811
coded15 <g.ichchha@iitg.ac.in>
2026-01-30 09:52:37 +0530

    major correction

--- CP/max_sum_subarray.cpp
+++ CP/max_sum_subarray.cpp
@@ -11,9 +11,16 @@ int main()
         cin >> A[i];
     }
     int max_sum = INT_MIN;
+    int first_sum = 0;
+    // dont get influenced by the test case and assume that the size of the subarray is k only
+    for (int i = 0; i < k; i++)
+    {
+        first_sum += A[i];
+    }
+    max_sum = first_sum;
     for (int i = 0; i < n - k; i++)
     {
-        max_sum = max((A[i] + A[i + 1] + A[i + 2]), max_sum);
+        max_sum = max(max_sum - A[i - 1] + A[i + k], max_sum);
     }
     cout << max_sum;
 }
